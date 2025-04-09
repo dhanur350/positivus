c4b8e7930361c81bb38f92e63a5ec93800d56f13
dhanur350 <dhanur297@gmail.com>
2025-04-09 12:34:23 +0530

    depepndency setup

---
+++ src/containers/HomeContainer/HomeContainer.tsx
@@ -0,0 +1,11 @@
+import { Header } from "@/components"
+
+function HomeContainer() {
+  return (
+    <div className="flex flex-col w-full h-full">
+      <Header />
+    </div>
+  )
+}
+
+export default HomeContainer
